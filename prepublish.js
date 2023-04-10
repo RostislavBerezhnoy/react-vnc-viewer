f437d9df01902172d78ff55250275e67b5ee6449
Rostislav Berezhnoy <berezhnoy.rostislav@gmail.com>
2023-04-10 15:56:19 +0300

    chore: fix

--- prepublish.js
+++ prepublish.js
@@ -1,28 +1,6 @@
 const fs = require('fs')
 const pkg = require('./package.json')
 
-// NOTE(roerohan): remove dependencies that are not needed for the library.
-const {
-  react,
-  'react-scripts': reactScripts,
-  'react-dom': reactDom,
-  '@testing-library/jest-dom': testingLibraryjestDom,
-  '@testing-library/react': testingLibraryReact,
-  '@testing-library/user-event': testingLibraryUserEvent,
-  '@types/jest': typesJest,
-  '@types/node': typesNode,
-  '@types/react': typesReact,
-  '@types/react-dom': typesReactDom,
-  'web-vitals': webVitals,
-  ...dependencies
-} = pkg.dependencies
-
-const peerDependencies = {
-  react: '>=16.0.0',
-  'react-scripts': '>=4.0.0',
-  'react-dom': '>=16.0.0',
-}
-
 fs.writeFileSync(
   'package.json',
   JSON.stringify(
@@ -34,11 +12,10 @@ fs.writeFileSync(
       keywords: pkg.keywords,
       main: pkg.main,
       module: pkg.module,
-      exports: pkg.exports,
-      typings: pkg.typings,
+      types: pkg.types,
       publishConfig: pkg.publishConfig,
-      dependencies,
-      peerDependencies,
+      dependencies: pkg.dependencies,
+      peerDependencies: pkg.peerDependencies,
     },
     null,
     4,
